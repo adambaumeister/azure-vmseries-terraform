f0f1b896fdc6ed2ef844ef05ca8a1d8956f2600e
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-10-15 09:50:21 +0200

    fix tf syntax warning

--- modules/test-vnet/testhost.tf
+++ modules/test-vnet/testhost.tf
@@ -95,7 +95,7 @@ resource "tls_private_key" "example_ssh" {
   rsa_bits  = 4096
 }
 output "tls_private_key" {
-  value = "${tls_private_key.example_ssh.private_key_pem}"
+  value = tls_private_key.example_ssh.private_key_pem
 }
 
 # Create virtual machine
