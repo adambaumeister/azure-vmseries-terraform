212fb05ee6e197675cd274a281363938ebd5a86f
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-10-15 10:29:20 +0200

    fix deprecated address_prefix

--- modules/test-vnet/testhost.tf
+++ modules/test-vnet/testhost.tf
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.name_prefix}-testhost-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.100.100.0/26"
+  address_prefixes     = ["10.100.100.0/26"]
 }
 
 # Create Network Security Group and rule
